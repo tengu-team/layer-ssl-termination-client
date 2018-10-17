a6c4911104378334f3d748413de2b9335e7b2347
sborny <sander.borny@ugent.be>
2018-10-17 10:46:21 +0200

    Added TCP functionality

--- reactive/ssl-termination-fqdn.py
+++ reactive/ssl-termination-fqdn.py
@@ -17,7 +17,14 @@
 import os
 from charmhelpers.core import unitdata
 from charmhelpers.core.hookenv import status_set, config
-from charms.reactive import when, when_not, set_flag, clear_flag, when_any
+from charms.reactive import (
+    when,
+    when_not,
+    set_flag,
+    clear_flag,
+    when_any,
+    is_flag_set,
+)
 from charms.reactive.relations import endpoint_from_flag
 
 
@@ -29,12 +36,14 @@ config = config()
 ########################################################################
 
 @when('endpoint.ssl-termination.available')
-@when_not('website.available')
-def missing_http_relation():
-    status_set('blocked', 'Waiting for http relation')
+@when_not('website.available',
+          'endpoint.tcp.joined')
+def missing_service_relation():
+    status_set('blocked', 'Waiting for http/tcp relation')
 
 
-@when('website.available')
+@when_any('website.available',
+          'endpoint.tcp.joined')
 @when_not('endpoint.ssl-termination.available')
 def missing_ssl_termination_relation():
     status_set('blocked', 'Waiting for ssl-termination-proxy relation')
@@ -51,49 +60,69 @@ def fqdns_changed():
 # Configure certificate
 ########################################################################
 
-@when('website.available',
-      'endpoint.ssl-termination.available')
+@when('endpoint.ssl-termination.available')
+@when_any('website.available',
+          'endpoint.tcp.joined')
 @when_not('client.cert-requested')
 def create_cert_request():
     if not config.get('fqdns'):
         status_set('blocked', 'Waiting for fqdns config')
         return
+    
     ssl_termination = endpoint_from_flag('endpoint.ssl-termination.available')
-    website = endpoint_from_flag('website.available')
-
-    services = website.services()
-    if not services:
-        return
-
     upstreams = []
-    for service in services:
-        upstreams.extend(service['hosts'])
+    tcps = []
+
+    if is_flag_set('website.available'):
+        website = endpoint_from_flag('website.available')
+
+        services = website.services()
+        if not services:
+            return        
+        for service in services:
+            upstreams.extend(service['hosts'])
+
+    if is_flag_set('endpoint.tcp.joined'):
+        tcp = endpoint_from_flag('endpoint.tcp.joined')
+        tcp_svcs = tcp.tcp_services()
+        tcps = transform_tcp_services(tcp_svcs)
+        if not tcps:
+            return
 
     ssl_termination.send_cert_info({
         'fqdn': config.get('fqdns').rstrip().split(),
         'contact-email': config.get('contact-email', ''),
         'credentials': config.get('credentials', ''),
         'upstreams': upstreams,
+        'tcp': tcps,
     })
     status_set('waiting', 'Waiting for proxy to register certificate')
     set_flag('client.cert-requested')
 
 
-@when('website.available',
-      'endpoint.ssl-termination.update')
+@when('endpoint.ssl-termination.available',
+      'client.cert-requested')
+@when_any('website.available',
+          'endpoint.tcp.joined',
+          'endpoint.ssl-termination.update')
 @when_not('client.cert-created')
 def check_cert_created():
-    ssl_termination = endpoint_from_flag('endpoint.ssl-termination.update')
+    ssl_termination = endpoint_from_flag('endpoint.ssl-termination.available')
     status = ssl_termination.get_status()
 
     # Only one fqdn will be returned for shared certs.
     # If any fqdn match, the cert has been created.
     match_fqdn = config.get('fqdns').rstrip().split()
     for unit_status in status:
+        if not unit_status['status']:
+            continue
         for fqdn in unit_status['status']:
             if fqdn in match_fqdn:
                 status_set('active', 'Ready')
                 set_flag('client.cert-created')
+
+    if is_flag_set('endpoint.ssl-termination.update'):
+        clear_flag('endpoint.ssl-termination.update')
 
 
 ########################################################################
@@ -102,8 +131,33 @@ def check_cert_created():
 
 @when('endpoint.ssl-termination.available',
       'client.cert-requested')
-@when_not('website.available')
+@when_not('website.available',
+          'endpoint.tcp.joined')
 def website_removed():
     endpoint = endpoint_from_flag('endpoint.ssl-termination.available')
     endpoint.send_cert_info({})
     clear_flag('client.cert-requested')
+    clear_flag('client.cert-created')
+
+
+########################################################################
+# Helper methods
+########################################################################
+
+def transform_tcp_services(tcp_services):
+    transform = []
+    for service in tcp_services:
+        host_ips = []
+        port = None
+        for host_port in service['hosts']:
+            host_ips.append(host_port['host'])
+            if port and port != host_port['port']:
+                status_set('blocked', 'Received multiple hosts from a single service with different ports.')
+                break
+            port = host_port['port']
+        transform.append({
+            'hosts': host_ips,
+            'port': port,
+        })
+    return transform
+
