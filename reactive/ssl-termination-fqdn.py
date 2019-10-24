c45d2d7a482282a4625c9c45afdcd9c04210c136
sborny <sander.borny@ugent.be>
2019-10-24 15:51:54 +0200

    Add support for bionic and nginx-config

--- reactive/ssl-termination-fqdn.py
+++ reactive/ssl-termination-fqdn.py
@@ -15,14 +15,13 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
-from charmhelpers.core import unitdata
 from charmhelpers.core.hookenv import status_set, config
 from charms.reactive import when, when_not, set_flag, clear_flag, when_any
 from charms.reactive.relations import endpoint_from_flag
+from charms.reactive.helpers import data_changed
 
 
 config = config()
-
 
 ########################################################################
 # Install
@@ -31,7 +30,6 @@ config = config()
 @when('endpoint.ssl-termination.available')
 @when_not('website.available')
 def missing_http_relation():
-    clear_flag('client.cert-created')
     status_set('blocked', 'Waiting for http relation')
 
 
@@ -42,10 +40,10 @@ def missing_ssl_termination_relation():
 
 
 @when_any('config.changed.fqdns',
-          'config.changed.credentials')
+          'config.changed.credentials',
+          'config.changed.nginx-config')
 def fqdns_changed():
-    clear_flag('client.cert-requested')
-    clear_flag('client.cert-created')
+    clear_cert_flags()
 
 
 ########################################################################
@@ -70,32 +68,37 @@ def create_cert_request():
     for service in services:
         upstreams.extend(service['hosts'])
 
-    ssl_termination.send_cert_info({
+    cert_request = {
         'fqdn': config.get('fqdns').rstrip().split(),
         'contact-email': config.get('contact-email', ''),
         'credentials': config.get('credentials', ''),
         'upstreams': upstreams,
-    })
+        'nginx-config': parse_nginx_config(),
+    }
+
+    ssl_termination.send_cert_info(cert_request)
     status_set('waiting', 'Waiting for proxy to register certificate')
     set_flag('client.cert-requested')
 
 
 @when('website.available',
-      'endpoint.ssl-termination.update')
+      'endpoint.ssl-termination.joined')
+@when_any('endpoint.ssl-termination.update',
+          'client.cert-requested')
 @when_not('client.cert-created')
 def check_cert_created():
-    ssl_termination = endpoint_from_flag('endpoint.ssl-termination.update')
+    ssl_termination = endpoint_from_flag('endpoint.ssl-termination.joined')
     status = ssl_termination.get_status()
-
     # Only one fqdn will be returned for shared certs.
     # If any fqdn match, the cert has been created.
     match_fqdn = config.get('fqdns').rstrip().split()
     for unit_status in status:
-        for fqdn in unit_status['status']:
-            if fqdn in match_fqdn:
-                status_set('active', 'Ready')
-                set_flag('client.cert-created')
-                clear_flag('endpoint.ssl-termination.update')
+        if unit_status['status']:
+            for fqdn in unit_status['status']:
+                if fqdn in match_fqdn:
+                    status_set('active', 'Ready')
+                    set_flag('client.cert-created')
+                    clear_flag('endpoint.ssl-termination.update')
 
 
 ########################################################################
@@ -103,9 +106,45 @@ def check_cert_created():
 ########################################################################
 
 @when('endpoint.ssl-termination.available',
-      'client.cert-requested')
+      'client.cert-created')
 @when_not('website.available')
 def website_removed():
     endpoint = endpoint_from_flag('endpoint.ssl-termination.available')
     endpoint.send_cert_info({})
+    clear_cert_flags()
+
+# Caution: changed flag is not set on departing units!
+# https://github.com/juju-solutions/charms.reactive/issues/170
+# Until this is resolved we need both website_updated_changed()
+# and website_updated_departed().
+@when('endpoint.website.changed',
+      'client.cert-created')
+def website_updated_changed():
+    clear_flag('endpoint.website.changed')
+    clear_cert_flags()
+
+
+@when('endpoint.website.departed',
+      'endpoint.website.joined',
+      'client.cert-created')
+def website_updated_departed():
+    endpoint = endpoint_from_flag('endpoint.website.departed')
+    clear_flag('endpoint.website.departed')
+    clear_cert_flags()
+
+
+########################################################################
+# Helper methods
+########################################################################
+
+def parse_nginx_config():
+    nginx_configs = []
+    for n_config in config.get('nginx-config', '').split(';'):
+        # Remove unnecessary whitespace, tabs and newlines
+        nginx_configs.append(' '.join(n_config.split()))    
+    return [x for x in nginx_configs if x] # Remove empty entries
+
+def clear_cert_flags():
     clear_flag('client.cert-requested')
+    clear_flag('client.cert-created')
+    
